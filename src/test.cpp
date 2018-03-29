f1fffb518ad6903e46b4269c2d6dec6ddf49317b
User <user@MacBooks-MacBook-Air.local>
2018-03-29 06:17:29 -0700

    basic file test.

--- src/test.cpp
+++ src/test.cpp
@@ -5,5 +5,15 @@ using namespace std;
 
 int main()
 {
+    ifstream handle;
+    handle.open("src/data.txt");
+
+    string line;
+
+    while(getline(handle,line))
+    {
+        cout << line << endl;
+    }
+
     return 0;
 }
