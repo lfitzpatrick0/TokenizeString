f3c428f77e57e43b20f0b94036071e1d0c8392d4
User <user@MacBooks-MacBook-Air.local>
2018-03-31 12:01:10 -0700

    added header for checking valid identifier.

---
+++ src/header.h
@@ -0,0 +1,30 @@
+#include <iostream>		//for i/o.
+#include <string>		//for string library.
+#include <cctype>		//for isdigit(), isalnum(), etc.
+using namespace std;
+
+bool isValid(string &str)
+{
+    bool valid = true;
+    if(isdigit(str[0]))
+        valid = false;
+    if(str.empty())
+        valid = false;
+    if(valid)
+    {
+        for(int i = 0; i < str.length(); i++)
+        {
+            while(str[i] == '_')
+                i++;
+            while(isalnum(str[i]))
+                i++;
+            if(isspace(str[i]) || (ispunct(str[i]) && str[i] != '_'))
+                valid = false;
+            else if(str[i] == '_')
+                i++;
+            else
+                valid = false;
+        }
+    }
+    return valid;
+}
